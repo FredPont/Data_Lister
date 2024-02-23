7350d90119c7ea7cef7963557109930680a4af23
FredPont <fred.softwares@gmail.com>
2024-02-23 08:23:15 +0100

    Readme & compilation update

--- compilation_all.sh
+++ compilation_all.sh
@@ -5,7 +5,7 @@ GOOS=windows GOARCH=amd64 CGO_ENABLED=1 CC=x86_64-w64-mingw32-gcc CXX=x86_64-w64
 
 
 echo "Start compilation for Mac 64bits"
-#GOOS=darwin go build -o Mac_DataLister.bin .
+#GOOS=darwin  GOARCH=amd64 go build -o Mac_DataLister.bin .
 
 
 echo "Start compilation for linux 64bits"
